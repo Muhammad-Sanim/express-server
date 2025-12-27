19d1b3c2b9fb30d9206436db410e1fd96683876f
Muhammad Sanim <sanim@Sanims-MacBook-Air.local>
2025-12-28 05:40:44 +0600

    second commit

--- src/server.ts
+++ src/server.ts
@@ -27,20 +27,19 @@ const initDB = async () => {
         address TEXT,
         created_at TIMESTAMP DEFAULT NOW(),
         update_at TIMESTAMP DEFAULT NOW()
-        ) `);
+        )`);
 
   await pool.query(`
           CREATE TABLE IF NOT EXISTS todos(
           id SERIAL PRIMARY KEY,
           user_id INT REFERENCES users(id) ON DELETE CASCADE,
-          title VARCHAR(255) NOT NULL,
+          title VARCHAR(200) NOT NULL,
           description TEXT,
           completed BOOLEAN DEFAULT FALSE,
           due_date DATE,
           created_at TIMESTAMP DEFAULT NOW(),
           updated_at TIMESTAMP DEFAULT NOW()
-          )
-          `);
+          )`);
 };
 
 initDB();
